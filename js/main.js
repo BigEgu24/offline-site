3ffd639427d0722e04b4c0c7bb02142405d596a8
BigEgu24 <BigEgu24@gmail.com>
2020-01-11 02:48:18 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
       .register('/offline-site.github.io/sw_cached_pages.js', {
-        scope: '/offline-site.github.io/'
+        scope: '/offline-site.github.io/sw_cached_pages.js'
       })
       .then(reg => console.log('Service Worker: Registered'))
       .catch(err => console.log(`Service Worker: Error: ${err}`));
