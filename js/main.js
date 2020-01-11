498ed48b0a4b1fd63102829b38c7345bafcd600b
BigEgu24 <BigEgu24@gmail.com>
2020-01-11 02:31:24 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
       .register('../sw_cached_pages.js', {
-        scope: 'https://bigegu24.github.io/offline-site.github.io/js/'
+        scope: '../sw_cached_pages.js'
       })
       .then(reg => console.log('Service Worker: Registered'))
       .catch(err => console.log(`Service Worker: Error: ${err}`));
